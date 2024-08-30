aac2713a8e9a2a5ec8e69af064d21018f16092de
upayanmazumder <upayanm3@gmail.com>
2024-08-30 19:16:51 +0530

    feat: Update temperature conversion script to improve readability and formatting
    Refactor the temperature conversion script in `c-to-k-&-f.py` to improve readability and formatting. Update the output message to use a consistent format and remove unnecessary newline characters.
    
    Also, update the prompt message in `tuition-fees.py` to be more concise and clear.
    
    These changes enhance the user experience and make the code more maintainable.

--- Python/vit/ex-4/tuition-fees.py
+++ Python/vit/ex-4/tuition-fees.py
@@ -1,3 +1,3 @@
-n=int(input("Enter number of subjects taken up: "))
+n=int(input("Number of subjects: "))
 cost=n*500
-print("Cost incurred on taking up {0} subjects is {1}".format(n,cost))
+print("Cost incurred for {0} subjects is Rs.{1}".format(n,cost))
