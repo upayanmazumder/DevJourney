b7d7460d627e4332638e602e7e301d2cf815907e
upayanmazumder <upayanm3@gmail.com>
2024-09-03 22:02:16 +0530

    feat: Add Java code for expressions and calculations
    This commit adds a new Java file `expressions.java` that demonstrates various expressions and calculations. It includes examples of incrementing and decrementing variables, as well as performing division calculations. This code will be helpful for beginners learning Java basics.

---
+++ Java/Basics/expressions.java
@@ -0,0 +1,20 @@
+package Java.Basics;
+
+public class expressions {
+
+    public static void main(String[] args) {
+        
+        double friends = 10;
+        friends = friends + 1;
+        System.out.println(friends);
+        friends++;
+        System.out.println(friends);
+        friends--;
+        System.out.println(friends);
+        
+        double cake = 1;
+        double cakeperfriends = (double)  cake / friends;
+        System.out.println(cakeperfriends);
+    }
+    
+}
