2ebb4820b768a7d80f93bb9f77d5782fc73c8d7c
upayanmazumder <upayanm3@gmail.com>
2024-09-03 14:00:32 +0530

    Update average and volume calculation scripts to remove unnecessary newline characters

--- Python/vit/ex-5/john's-expenses.py
+++ Python/vit/ex-5/john's-expenses.py
@@ -1,8 +1,6 @@
-# Define John's annual salary and percentage for gifts
 s = 1200000
 giftP = 10
 
-# Calculate the allotted amount for gifts
 giftT = (giftP / 100) * s
 
 # Amount spent on gifts
@@ -10,10 +8,9 @@ mDay = 25000
 vDay = 50000
 bDay = 15000
 
-# Calculate total expenditure on gifts
+# Calculate total expenditure
 totExp = mDay +vDay + bDay
 
-# Check if expenditure exceeds the allotted amount
 if totExp > giftT:
     print("John's expenditure on gifts has exceeded the allotted amount.")
 else:
