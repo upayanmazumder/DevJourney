d691bf6fdf32287cc94a8db231273c26b60805fa
upayanmazumder <upayanm3@gmail.com>
2024-09-22 19:50:54 +0530

    Refactor variable names and update prime number transfer logic in Python script

--- Python/VIT/ex-10/5. Transfer Prime Numbers from a list to another list/transfer primer numbers from a list to a new one.py
+++ Python/VIT/ex-10/5. Transfer Prime Numbers from a list to another list/transfer primer numbers from a list to a new one.py
@@ -1,5 +1,5 @@
 n=10
-L1,L2=[],[]
+A1,A2=[],[]
 
 # Get user input
 for i in range(n):
@@ -7,18 +7,18 @@ for i in range(n):
     while x<=0:
         print("Please enter a positive number!")
         x=int(input(f'Enter number {i+1}: '))
-    L1.append(x)
+    A1.append(x)
 
 # Filter and store the prime numbers              
 for i in range(n): # For each number the user entered
     prime=True # Initially assume its prime
-    for j in range(2,L1[i]): # Start a iterative check to find whether at least one number can divide it
-        if L1[i]%j==0:
+    for j in range(2,A1[i]): # Start a iterative check to find whether at least one number can divide it
+        if A1[i]%j==0:
             prime=False
-    if prime and L1[i] not in L2: # If no factors were found and the number isnt already stored in new list
-        L2.append(L1[i]) 
+    if prime and A1[i] not in A2: # If no factors were found and the number isnt already stored in new list
+        A2.append(A1[i]) 
 
-if len(L2)>0:
-    print(f'Original List: {L1}\nNew List (PRIME): {L2}')
+if len(A2)>0:
+    print(f'Original List: {A1}\nNew List (PRIME): {A2}')
 else:
     print('No prime numbers')
