9c38d98859ccdb0c521336a87ac71a5b81ce11d7
upayanmazumder <upayanm3@gmail.com>
2024-09-30 20:32:07 +0530

    finished ex 11

--- Python/VIT/ex-11/5. Combine two dictionaries with common keys/combine two dictionaries with common keys.py
+++ Python/VIT/ex-11/5. Combine two dictionaries with common keys/combine two dictionaries with common keys.py
@@ -1,15 +1,16 @@
 d1 = { 'a' : 100, 'b' : 200, 'c' : 300 }
 d2 = { 'a' : 300, 'b' : 200, 'd' : 400 }
-di={}
-for k1 in d1.keys():  # For each key in d1
-    for k2 in d2.keys():  # For each key in d2
-        foundk2=False # Initialize found to False
-        di.clear() # Initialise mini dictionary
-        if k1 == k2:    # If the keys are the same
-            d1[k1] += d2[k2]    # Add the values of the keys and store in d1
-            foundk2=True # Set found to True for the key in d2
-        if not foundk2: # If foundk2 is False
-            di.setdefault(k2,d2[k2]) # If found is True, add the key and value to dictionary
-            d1.update(di)  # Update d1 with the key and value in di
+d3 = {}
 
-# Incomplete
+# First make a list d3 with common keys
+d3.update(d1)
+d3.update(d2)
+
+# Update the values of common keys
+for i in d1.keys():
+    for j in d2.keys():
+        if i == j:
+            d3[i] += d1[i]
+
+# Print final dictionary
+print(d3)
