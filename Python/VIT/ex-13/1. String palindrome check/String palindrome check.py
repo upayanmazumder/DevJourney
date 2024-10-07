7ac1403066b38dbfd52d02c1e13d0b4934ac281b
upayanmazumder <upayanm3@gmail.com>
2024-10-08 01:11:29 +0530

    updates

--- Python/VIT/ex-13/1. String palindrome check/String palindrome check.py
+++ Python/VIT/ex-13/1. String palindrome check/String palindrome check.py
@@ -1,6 +1,7 @@
 word=str(input("Enter a string: "))
-wordR=word[::-1]
-if word==wordR:
+wordF=word.lower()
+wordR=wordF[::-1]
+if wordF==wordR:
     print("Palindrome")
 else:
     print("Not a palindrome")
