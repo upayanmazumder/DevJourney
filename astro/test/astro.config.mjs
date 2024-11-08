49019838012728c6633f8ca4c90d6529d0907e78
upayanmazumder <upayanm3@gmail.com>
2024-11-08 22:44:32 +0530

    Add React integration and update dependencies in Astro project

--- astro/test/astro.config.mjs
+++ astro/test/astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import deno from '@deno/astro-adapter';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: deno(),
+  output: 'server',
+  adapter: deno(),
+  integrations: [react()],
 });
