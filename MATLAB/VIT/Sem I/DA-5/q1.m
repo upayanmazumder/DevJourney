3317d1ee80334543f7155c9214000b8b4117cb1f
upayanmazumder <upayanm3@gmail.com>
2024-11-19 23:48:08 +0530

    Fixes

--- MATLAB/VIT/Sem I/DA-5/q1.m
+++ MATLAB/VIT/Sem I/DA-5/q1.m
@@ -1,7 +1,15 @@
 syms x y z
-V = [x, 2*y^2, 3*z^2];
-div_curl_V = divergence(curl(V, [x, y, z]), [x, y, z]);
-disp(simplify(div_curl_V));
-[x, y, z] = meshgrid(-2:1:2);
-quiver3(x, y, z, x, 2*y.^2, 3*z.^2);
-xlabel( 'x'); ylabel ('y' ); zlabel('z ' ); title('Vector Field V'); 
+V = [x, 2*y^2, 3*z^3];
+curl_V = curl(V, [x,y,z]);
+div_curl_V = divergence(curl_V, [x,y,z]);
+disp('Divergence of the curl: ');
+disp(div_curl_V);
+[X, Y, Z] = meshgrid(-2:0.5:2, -2:0.5:2, -2:0.5:2);
+U = X; V = 2*Y.^2; W = 3*Z.^3;
+figure;
+quiver3(X,Y,Z,U,V,W);
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('Vector Field V(x, y, z)');
+grid on;
