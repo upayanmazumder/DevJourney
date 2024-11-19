b22dc4d52ff3e0bf7a8046707cb2e06b0497cdd2
upayanmazumder <upayanm3@gmail.com>
2024-11-20 00:12:33 +0530

    massive fix

--- MATLAB/VIT/Sem I/DA-3/q1.m
+++ MATLAB/VIT/Sem I/DA-3/q1.m
@@ -1,10 +1,20 @@
-syms x y z L
-f(x,y,z)=x*y*z;
-g(x,y,z)=2*(x*y+y*z+z*x)-64;
-F=f+L*g;
-Fx=diff(F,x);Fy=diff(F,y);Fz=diff(F,z);
-S=solve(g,Fx,Fy,Fz,'Real',true);
-St_pts=[S.x,S.y,S.z];
-St_pts=double(St_pts);
-Fun_Val=f(S.x,S.y,S.z);
-Fun_Val=double(Fun_Val) 
+surface_area = 64;
+
+syms x y
+h = (32 - x * y) / (x + y)
+V = x * y * h;
+
+dV_dx = diff(V, x);
+dV_dy = diff(V, y);
+
+[sol_x, sol_y] = solve(dV_dx == 0, dV_dy == 0, x > 0, y > 0);
+
+sol_h = (32 - sol_x * sol_y) / (sol_x + sol_y);
+
+fprintf('Optimal Dimensions: \n');
+fprintf('Length (x): %.2f cm\n', double(sol_x));
+fprintf('Width (y): %.2f cm\n', double(sol_y));
+fprintf('Height (h): %.2f cm\n', double(sol_h));
+
+max_volume = double(sol_x * sol_y * sol_h);
+fprintf('Maximum Volume: %.2f cm³\n', max_volume)
