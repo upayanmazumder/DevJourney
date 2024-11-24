e08b5fc9da1babb4ea53131b8aabeba3404cafdf
upayanmazumder <upayanm3@gmail.com>
2024-11-24 12:22:40 +0530

    🛠️ Python/RegEx/Count numbers of vowels and consonants in the file and append the result to the file/file.txt -> Added vowel and consonant counts.
    🛠️ Python/RegEx/Count numbers of vowels and consonants in the file and append the result to the file/main.py -> Changed file write mode to append.

--- Python/RegEx/Count numbers of vowels and consonants in the file and append the result to the file/main.py
+++ Python/RegEx/Count numbers of vowels and consonants in the file and append the result to the file/main.py
@@ -6,12 +6,11 @@ with open("file.txt", "r") as content:
     Consonants = 0
 
     for line in content:
-        print(line, end='')
         vCount = len(re.findall(r'[aeiouAEIOU]',line))
         cCount = len(re.findall(r'[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]',line))
         Vowels += vCount
         Consonants += cCount
 
-with open("file.txt",'w') as content:
-    res = f"Vowels => {Vowels}\nConsonants => {Consonants}"
+with open("file.txt",'a') as content:
+    res = f"\nVowels => {Vowels}\nConsonants => {Consonants}"
     content.write(res)
