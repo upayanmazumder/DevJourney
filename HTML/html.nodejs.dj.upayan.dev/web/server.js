f316249449c8777c9a85e19ff294555071c3a228
Upayan Mazumder <upayanm3@gmail.com>
2024-12-17 17:52:40 +0530

    Refactor Dockerfile and server.js for improved clarity and error handling

--- HTML/html.nodejs.dj.upayan.dev/web/server.js
+++ HTML/html.nodejs.dj.upayan.dev/web/server.js
@@ -2,14 +2,14 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '0.0.0.0';
+const hostname = `0.0.0.0`;
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log(`Request for ${req.url} received.`);
 
     let filePath = '.' + req.url;
-    if (filePath == './') {
+    if (filePath === './') {
         filePath = './index.html';
     }
 
@@ -35,15 +35,19 @@ const server = http.createServer((req, res) => {
 
     fs.readFile(filePath, (error, content) => {
         if (error) {
-            if (error.code == 'ENOENT') {
+            if (error.code === 'ENOENT') {
                 fs.readFile('./404.html', (error, content) => {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf-8');
+                    if (error) {
+                        res.writeHead(500);
+                        res.end('Error loading 404 page');
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end(content, 'utf-8');
+                    }
                 });
             } else {
                 res.writeHead(500);
                 res.end(`Sorry, check with the site admin for error: ${error.code} ..\n`);
-                res.end();
             }
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
@@ -53,5 +57,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}`);
 });
