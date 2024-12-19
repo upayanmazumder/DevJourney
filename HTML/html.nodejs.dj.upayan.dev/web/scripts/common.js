564a898c2c991c46e78afdcfcb0480faf5536cf0
Upayan Mazumder <upayanm3@gmail.com>
2024-12-19 16:42:07 +0530

    Add 404 error page, enhance forms and table pages with headings, and implement smooth scrolling in JavaScript

--- HTML/html.nodejs.dj.upayan.dev/web/scripts/common.js
+++ HTML/html.nodejs.dj.upayan.dev/web/scripts/common.js
@@ -15,3 +15,34 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error loading footer:', error));
 });
+
+function smoothScroll(target, duration) {
+    var targetElement = document.querySelector(target);
+    var targetPosition = targetElement.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, targetPosition, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        smoothScroll(this.getAttribute('href'), 1000);
+    });
+});
