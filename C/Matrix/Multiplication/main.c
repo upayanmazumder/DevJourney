d4bd3c726004ca6411501c8e4f3207b4120cfd2c
Upayan Mazumder <upayanm3@gmail.com>
2025-01-02 17:42:02 +0530

    Add matrix addition and multiplication programs

---
+++ C/Matrix/Multiplication/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int a[10][10],b[10][10],i,j,m,n;
+    printf("Enter the number of rows and columns of the matrix: ");
+    scanf("%d %d",&m,&n);
+    printf("Enter the elements of the first matrix: ");
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+    printf("Enter the elements of the second matrix: ");
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&b[i][j]);
+    printf("The product of the two matrices is: \n");
+    for(i=0;i<m;i++) {
+        for(j=0;j<n;j++)
+            printf("%d ",a[i][j]*b[i][j]);
+        printf("\n");
+    }
+}
