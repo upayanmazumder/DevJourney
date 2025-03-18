cd15f3451ca4a5cc70ada6ae4da1c8d1f14ae758
Upayan Mazumder <upayanm3@gmail.com>
2025-03-18 22:27:29 +0530

    Added error handling

--- C/Pointers/Calculator/main.c
+++ C/Pointers/Calculator/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int x, y, res;
     char mode, *m = &mode;
     int *a = &x, *b = &y, *c = &res;
-    
+    bool valid = 1;
+
     printf("Enter [num1] [operation] [num2]: ");
     scanf("%d %c %d", &x, &mode, &y);
     
@@ -22,8 +24,12 @@ int main() {
             res = x / y;
             break;
         default:
+            valid = false;
             break;
     }
     
-    printf("%d %c %d = %d", x, mode, y, res);
+    if (valid)
+        printf("%d %c %d = %d", x, mode, y, res);
+    else
+        printf("Invalid!");
 }
