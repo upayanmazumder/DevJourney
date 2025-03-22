6fcf7a2aa587e1d255f6b6394603fde1d62a6c2d
Upayan Mazumder <upayanm3@gmail.com>
2025-03-23 00:39:42 +0530

    Reset NextJS

--- nextjs/learning-nextjs/src/app/layout.js
+++ nextjs/learning-nextjs/src/app/layout.js
@@ -1,17 +1,14 @@
-import localFont from "next/font/local";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header/header";
-import Footer from "../components/footer/footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const geistSans = Geist({
   variable: "--font-geist-sans",
-  weight: "100 900",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  weight: "100 900",
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -23,9 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- nextjs/learning-nextjs/src/components/header/header.js
+++
@@ -1,9 +0,0 @@
-import styles from './header.module.css';
-
-export default function Header() {
-  return (
-    <div className={styles.header}>
-      <h1>Welcome to My Next.js App</h1>
-    </div>
-  );
-}
