b140831fe7a21eb5771248cbadd050f8e78e4f2f
Upayan M <upayanm3@gmail.com>
2025-05-06 18:16:53 +0530

    Did a bit

--- qwik/src/root.tsx
+++ qwik/src/root.tsx
@@ -5,7 +5,6 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-import { isDev } from "@builder.io/qwik";
 
 import "./global.css";
 
@@ -14,24 +13,19 @@ export default component$(() => {
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
    *
-   * Don't remove the `<head>` and `<body>` elements.
+   * Don't remove the '<head>' and '<body>' elements.
    */
 
   return (
     <QwikCityProvider>
       <head>
-        <meta charset="utf-8" />
-        {!isDev && (
-          <link
-            rel="manifest"
-            href={`${import.meta.env.BASE_URL}manifest.json`}
-          />
-        )}
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
-        {!isDev && <ServiceWorkerRegister />}
+        <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
   );
