9d7cd0f7224405bdd4efcb3c4a05d5eed1432cfe
Upayan M <upayanm3@gmail.com>
2025-06-21 21:50:11 +0530

    gg

--- react/learning-react/app/src/App.js
+++ react/learning-react/app/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <a href="https://upayan.dev" target="_blank" rel="noopener noreferrer">
           upayan.dev
         </a>
-        native
+        native with secret
       </header>
     </div>
   );
