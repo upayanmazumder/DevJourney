e66dae48052bc45d895155c141a1787a4c8d8d3f
Upayan M <upayanm3@gmail.com>
2025-06-21 17:30:05 +0530

    gg

--- react/learning-react/app/src/App.js
+++ react/learning-react/app/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <a href="https://upayan.dev" target="_blank" rel="noopener noreferrer">
           upayan.dev
         </a>
-        This uses polling haha
+        This uses polling haha uwu
       </header>
     </div>
   );
