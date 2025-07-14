8d87fd0c7c501738331a74896d2c5a8ca8899505
Upayan M <upayanm3@gmail.com>
2025-07-14 20:58:54 +0530

    Add various C programs for array manipulations including sorting, finding minimum/maximum, and removing duplicates

---
+++ CPP/DSA/TUF/2. Patterns/Pattern-1 Rectangular Star Pattern/main.c
@@ -0,0 +1,9 @@
+void nForest(int n) {
+	int i = 0, j = 0;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			cout << "* ";
+		}
+		printf("\n");
+	}
+}
---
+++ CPP/DSA/TUF/2. Patterns/Pattern-2 Right-Angled Triangle Pattern/main.c
@@ -0,0 +1,7 @@
+void nForest(int n) {
+    string line;
+    for (int i = 1; i <= n; i++) {
+        line += "* ";
+        cout << line << '\n';
+    }
+}
