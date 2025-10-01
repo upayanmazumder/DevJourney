fb064e8b6cf024a8bf8986fd7b845dd53fa8a170
Upayan M <upayanm3@gmail.com>
2025-10-02 03:56:08 +0530

    Updates

---
+++ C/Linked Lists/Basics/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *createNode(int value)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtHead(struct Node **head, int value)
+{
+    struct Node *newNode = createNode(value);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAtEnd(struct Node **head, int value)
+{
+    struct Node *newNode = createNode(value);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    struct Node *temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node *head)
+{
+    struct Node *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main()
+{
+    struct Node *head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 20);
+    insertAtHead(&head, 5);
+    insertAtEnd(&head, 30);
+
+    printList(head);
+
+    freeList(head);
+    return 0;
+}
