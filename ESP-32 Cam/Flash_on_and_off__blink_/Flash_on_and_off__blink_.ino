2ae34ca2b5d939b28ed091cfa26e540c37ea8ab8
Upayan M <upayanm3@gmail.com>
2025-10-12 03:16:33 +0530

    fix

--- ESP-32 Cam/Flash_on_and_off__blink_/Flash_on_and_off__blink_.ino
+++ ESP-32 Cam/Flash_on_and_off__blink_/Flash_on_and_off__blink_.ino
@@ -1,15 +1,17 @@
 // Simple ESP32-CAM Test
 // Blinks the flash LED and prints to Serial
 
-#define LED_FLASH 4  // Onboard flash LED on ESP32-CAM (AI Thinker)
+#define LED_FLASH 4 // Onboard flash LED on ESP32-CAM (AI Thinker)
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(LED_FLASH, OUTPUT);
   Serial.println("ESP32-CAM Test Started");
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Turning LED ON");
   digitalWrite(LED_FLASH, HIGH);
   delay(1000);
@@ -17,12 +19,4 @@ void loop() {
   Serial.println("Turning LED OFF");
   digitalWrite(LED_FLASH, LOW);
   delay(1000);
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
