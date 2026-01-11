5d61442c24ef6e5f179ea42ddb31d42b749e4f3b
Upayan <upayanm3@gmail.com>
2026-01-12 00:04:57 +0530

    Experiment 4

---
+++ 8051/Task-1/Experiment-4/T1E4P2.asm
@@ -0,0 +1,24 @@
+ORG 0000H
+    MOV DPTR, #0200H
+    MOV R0, #10
+
+LOOP: 
+    CLR A
+    MOVC A, @A + DPTR
+    ADD A, R2
+    DA A
+    JNC NEXT
+    INC R3
+
+NEXT: 
+    INC DPTR
+    MOV R2, A
+    DJNZ R0, LOOP
+
+HERE: 
+    SJMP HERE
+
+ORG 0200H
+    DB 22H, 43H, 23H, 34H, 31H, 77H, 91H, 33H, 43H, 07H
+
+END
