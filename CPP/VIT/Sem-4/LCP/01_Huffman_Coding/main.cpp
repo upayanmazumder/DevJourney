71ecb1d05c537a43f720372fa264f836ead57f4d
Upayan <upayanm3@gmail.com>
2026-03-11 02:11:36 +0530

    refactor: replace priority queue with manual node management in Huffman coding implementation

--- CPP/VIT/Sem-4/LCP/01_Huffman_Coding/main.cpp
+++ CPP/VIT/Sem-4/LCP/01_Huffman_Coding/main.cpp
@@ -1,56 +1,83 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <string>
 using namespace std;
+
 struct Node
 {
     string sym;
     int freq;
     Node *l, *r;
-    Node(string s, int f) : sym(s), freq(f), l(nullptr), r(nullptr) {}
+
+    Node(string s, int f)
+    {
+        sym = s;
+        freq = f;
+        l = nullptr;
+        r = nullptr;
+    }
 };
-struct Cmp
-{
-    bool operator()(Node *a, Node *b) const { return a->freq > b->freq; }
-};
+
 void printCodes(Node *root, string code)
 {
     if (!root)
         return;
+
     if (!root->l && !root->r)
     {
         cout << root->sym << ": " << code << "\n";
         return;
     }
-    printCodes(root->l, code + '0');
-    printCodes(root->r, code + '1');
+
+    printCodes(root->l, code + "0");
+    printCodes(root->r, code + "1");
 }
+
+int findMin(vector<Node *> &nodes)
+{
+    int idx = 0;
+    for (int i = 1; i < nodes.size(); i++)
+    {
+        if (nodes[i]->freq < nodes[idx]->freq)
+            idx = i;
+    }
+    return idx;
+}
+
 int main()
 {
     int n;
-    if (!(cin >> n))
-        return 0;
-    priority_queue<Node *, vector<Node *>, Cmp> pq;
+    cin >> n;
+
+    vector<Node *> nodes;
+
     for (int i = 0; i < n; i++)
     {
         string s;
         int f;
         cin >> s >> f;
-        pq.push(new Node(s, f));
+        nodes.push_back(new Node(s, f));
     }
-    while (pq.size() > 1)
+
+    while (nodes.size() > 1)
     {
-        Node *a = pq.top();
-        pq.pop();
-        Node *b = pq.top();
-        pq.pop();
+        int i1 = findMin(nodes);
+        Node *a = nodes[i1];
+        nodes.erase(nodes.begin() + i1);
+
+        int i2 = findMin(nodes);
+        Node *b = nodes[i2];
+        nodes.erase(nodes.begin() + i2);
+
         Node *p = new Node("", a->freq + b->freq);
         p->l = a;
         p->r = b;
-        pq.push(p);
+
+        nodes.push_back(p);
     }
-    if (!pq.empty())
-        printCodes(pq.top(), "");
+
+    if (!nodes.empty())
+        printCodes(nodes[0], "");
+
     return 0;
 }
